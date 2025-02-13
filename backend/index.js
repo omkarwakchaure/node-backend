d3dfc347f95b1ba7a9ca668a54aca24dba1a4f63
Omkar Wakchaure <omkarwakchaure2019@gmail.com>
2025-02-13 09:36:53 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      question: "What do you call a bear with no teeth?",
+      answer: "A gummy bear",
+    },
+    {
+      id: 2,
+      question: "What do you call a bear with no teeth?",
+      answer: "A gummy bear",
+    },
+    {
+      id: 3,
+      question: "What do you call a bear with no teeth?",
+      answer: "A gummy bear",
+    },
+  ];
+  res.send(jokes);
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
